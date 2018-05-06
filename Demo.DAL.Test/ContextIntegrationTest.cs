bf482cb7aa618b0296b0728d89313ce5a54af5b3
Anton Lyhin <an.lygin@gmail.com>
2018-05-06 20:27:12 +0300

    v0.002 - integration test for creating db from DemoContext;

---
+++ Demo.DAL.Test/ContextIntegrationTest.cs
@@ -0,0 +1,35 @@
+﻿using Demo.DAL.Context;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Demo.DAL.Test
+{
+    [TestClass]
+    public class ContextIntegrationTest
+    {
+        [TestMethod]
+        public void CreateDbSchema()
+        {
+            using (var ctx = new DemoContext())
+            {
+                var human = new Model.Human
+                {
+                    Id = 0,
+                    Description = "Description",
+                    Name = "Name"
+                };
+
+                try
+                {
+                    // ctx.Entities<Model.Human>().Add(human);
+                    ctx.Humans.Add(human);
+                    ctx.Save();
+                }
+                catch (Exception e)
+                {
+                    var x = 1;
+                }
+            }
+        }
+    }
+}
